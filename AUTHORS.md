bfe6ed6a2eb86c8338f2af4d9a97665375fe8c21
Lukas Sykora <lukassykora@seznam.cz>
2021-10-29 00:04:03 +0200

    Cookiecutter template for PyApriori

---
+++ AUTHORS.md
@@ -0,0 +1,7 @@
+# Credits
+
+## Development Lead
+- Lukas Sykora <lukassykora@seznam.cz>
+
+## Contributors
+None yet. Why not be the first?
---
+++ pyapriori/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for PyApriori."""
+
+__author__ = """Lukas Sykora"""
+__email__ = 'lukassykora@seznam.cz'
+__version__ = '0.1.0'
