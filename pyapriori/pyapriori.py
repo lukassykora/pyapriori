fa6d373aa5aade7ffaf1c596f7b28c26b3704f5c
Lukas Sykora <lukas.sykora@robolytix.com>
2022-03-05 23:29:54 +0100

    Big change

--- pyapriori/pyapriori.py
+++ pyapriori/pyapriori.py
@@ -1,11 +1,10 @@
 """Main module."""
+from typing import List
 from pyapriori.utils.utils import (
     frequent_single_itemsets,
     generate_candidates,
     itemsets_support,
     min_support_set,
-    Data,
-    get_numpy_or_cupy,
 )
 
 
@@ -16,7 +15,7 @@ class PyApriori:
         self.min_support = min_support
         self.min_length = min_length
 
-    def fit(self, data: Data) -> tuple:
+    def fit(self, data: List[set]) -> tuple:
         """
 
         Parameters
@@ -28,48 +27,39 @@ class PyApriori:
         -------
 
         """
-        numpy_or_cupy = get_numpy_or_cupy(data)
-
         candidates, candidates_support, data = frequent_single_itemsets(
             data, self.min_support
         )
         k = 2
-        multiplier_mask_left = None
-        result = None
-        result_support = None
+        multiplier_mask = None
+        result = []
+        result_support = []
         if self.min_length < 2:
             result = candidates
             result_support = candidates_support
-        while candidates.size > 0:
-            multiplier_mask_left, multiplier_mask_right = generate_candidates(
-                candidates, multiplier_mask_left
+        while len(candidates) > 0:
+            multiplier_mask = generate_candidates(
+                candidates, multiplier_mask
             )
-            if len(multiplier_mask_left) == 0:
+            if len(multiplier_mask) == 0:
                 break
             data, candidates_support = itemsets_support(
-                data, multiplier_mask_left, multiplier_mask_right
+                data, multiplier_mask
             )
             (
                 candidates,
                 candidates_support,
-                multiplier_mask_left,
+                multiplier_mask,
                 data,
             ) = min_support_set(
                 candidates,
                 candidates_support,
                 data,
-                multiplier_mask_left,
-                multiplier_mask_right,
+                multiplier_mask,
                 self.min_support,
             )
             if k >= self.min_length:
-                if result is not None:
-                    result = numpy_or_cupy.concatenate((result, candidates), axis=0)
-                    result_support = numpy_or_cupy.concatenate(
-                        (result_support, candidates_support), axis=0
-                    )
-                else:
-                    result = candidates
-                    result_support = candidates_support
+                result += candidates
+                result_support += candidates_support
             k += 1
         return result, result_support
--- pyapriori/utils/utils.py
+++ pyapriori/utils/utils.py
@@ -1,30 +1,26 @@
 from itertools import combinations
-from typing import Union, Tuple, List, Set
+from typing import Tuple, List
 
-import cupy as cp
 import numpy as np
 
-Data = dict[int, Set[int]]
-MultiDimensionalArray = Union[np.ndarray, cp.ndarray]
 
-
-def get_support(data: Data) -> List[int]:
+def get_support(data: List[set]) -> List[int]:
     """Get support
 
     Parameters
     ----------
-    data: Data :
+    data: List[set] :
 
     Returns
     -------
 
     """
-    return [len(value) for value in data.values()]
+    return [len(value) for value in data]
 
 
 def frequent_single_itemsets(
-    data: Data, min_support: int = 0
-) -> Tuple[MultiDimensionalArray, MultiDimensionalArray, Data]:
+    data: List[set], min_support: int = 0
+) -> Tuple[List[set], List[int], List[set]]:
     """
 
     Parameters
@@ -42,16 +38,17 @@ def frequent_single_itemsets(
     """
     columns_support = get_support(data)
     # Reduce by support
-    data = {feature_key: data[feature_key] for feature_key, support in
-            enumerate(columns_support) if support >= min_support}
+    data = [data[i] for i, support in enumerate(columns_support) if
+            support >= min_support]
     support = [support for support in columns_support if support >= min_support]
-    return support, data
+    candidates = [set(i) for i, support in enumerate(columns_support) if
+                  support >= min_support]
+    return candidates, support, data
 
 
 def generate_candidates(
-    previous_candidates: List[int],
-    previous_multiplier_mask: List[int] = None,
-) -> Tuple[List[int], List[int]]:
+    previous_candidates: List[set], previous_multiplier_mask: List[tuple] = None
+) -> List[tuple]:
     """Generate candidate set from `previous_candidates` with size `k`
 
     Parameters
@@ -65,87 +62,75 @@ def generate_candidates(
     -------
 
     """
+    if previous_multiplier_mask is not None:
+        previous_multiplier_mask = [a for a, b in previous_multiplier_mask]
 
     # number of previous candidates
     d = len(previous_candidates)
 
     # If no previous candidates then return empty arrays
     if d <= 1:
-        return [], []
+        return []
 
     if previous_multiplier_mask is None:  # Generate candidates with size 2
-        perm = combinations(range(0, d), 2)
-        multiplier_mask_left, multiplier_mask_right = map(list, zip(*perm))
+        perm_all = list(combinations(range(0, d), 2))
     else:  # Generate candidates with size > 2
         count_arr = np.bincount(previous_multiplier_mask)
         no_zeros_mask = count_arr > 0
         count_arr = count_arr[no_zeros_mask]
         cum_n = np.cumsum(count_arr)
         cum_n = np.concatenate((np.array([0]), cum_n))
-        cum_n = cum_n[:-1].copy()
-        multiplier_mask_left = []
-        multiplier_mask_right = []
+        perm_all = []
         if len(count_arr) == 0:
-            return [], []
+            return []
         for i, count in enumerate(count_arr):
             if count == 1:
                 continue
             offset = cum_n[i]
-            perm = combinations(range(offset, offset + count), 2)
-            multiplier_mask_left_part, multiplier_mask_right_part = map(
-                list, zip(*perm)
-            )
-            multiplier_mask_left += multiplier_mask_left_part
-            multiplier_mask_right += multiplier_mask_right_part
-
-    return multiplier_mask_left, multiplier_mask_right
+            perm = list(combinations(range(offset, offset + count), 2))
+            perm_all = perm_all + perm
+    return perm_all
 
 
 def itemsets_support(
-    data: Data, multiplier_mask_left: List[int], multiplier_mask_right: List[int]
-) -> Tuple[Data, MultiDimensionalArray]:
+    data: List[set], multiplier_mask: List[tuple]
+) -> Tuple[List[set], List[int]]:
     """Get support for `itemsets` and return sets with minimal `support
 
     Parameters
     ----------
     data: Data :
 
-    multiplier_mask_left: List[int] :
-
-    multiplier_mask_right: List[int] :
+    multiplier_mask: List[tuple] :
 
 
     Returns
     -------
 
     """
-    data = {i: data[left].intersection(data[multiplier_mask_right[i]]) for i, left in
-            enumerate(multiplier_mask_left)}
+    data = [data[left].intersection(data[right]) for left, right in multiplier_mask]
     data_support = get_support(data)
     return data, data_support
 
 
 def min_support_set(
-    previous_candidates: np.ndarray,
-    candidates_support: MultiDimensionalArray,
-    data: Data,
-    multiplier_mask_left: List[int],
-    multiplier_mask_right: List[int],
+    previous_candidates: List[set],
+    candidates_support: List[int],
+    data: List[set],
+    multiplier_mask: List[tuple],
     min_support: int = 0,
-) -> Tuple[MultiDimensionalArray, MultiDimensionalArray, List[int], Data]:
+) -> Tuple[List[set], List[int], List[tuple], Data]:
     """
 
     Parameters
     ----------
-    previous_candidates: np.ndarray :
+    previous_candidates: List[set] :
 
-    candidates_support:MultiDimensionalArray :
+    candidates_support: List[int] :
 
-    data: Data :
+    data: List[set] :
 
-    multiplier_mask_left:List[int] :
-
-    multiplier_mask_right:List[int] :
+    multiplier_mask: List[tuple] :
 
     min_support: int :
          (Default value = 0)
@@ -154,12 +139,18 @@ def min_support_set(
     -------
     type
 
-
     """
     # Reduce by support
-    data = {feature_key: data[feature_key] for feature_key, support in
-            enumerate(candidates_support) if support >= min_support}
-    candidates_support = [support for support in candidates_support if support >= min_support]
+    mask = [i for i, support in enumerate(candidates_support) if
+            support >= min_support]
 
+    candidates = [candidate for i, candidate in enumerate(previous_candidates) if
+                  i in mask]
+    candidates_support = [support for i, support in enumerate(candidates_support) if
+                          i in mask]
+    data = [value for i, value in enumerate(data) if
+            i in mask]
+    multiplier_mask = [value for i, value in enumerate(multiplier_mask) if
+                       i in mask]
 
-    return candidates, candidates_support, multiplier_mask_left, data
+    return candidates, candidates_support, multiplier_mask, data
